71f0bc7a07c37d13d1ec1eec9ee0c2682ba3c1d9
Ilia Vazhenin <heyheyftw@gmail.com>
2024-10-09 18:23:48 +0400

    inital +  3 pages routes

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <main class="main">
+    <RouterView />
+    <nav>
+      <RouterLink to="/">Add Transaction</RouterLink>
+      <RouterLink to="/overview">Overview</RouterLink>
+      <RouterLink to="/settings">Settings</RouterLink>
+    </nav>
+  </main>
+
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+</style>
---
+++ src/pages/AddTransactionPage.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="main">
+    <section class="general">
+      
+      <h1>Balance</h1>
+      <div class="balance-row">
+        <span class="account-name"></span>
+        <span class="account-sum"></span>
+      </div>
+
+      <h2>Monthly</h2>
+      <div class="monthly-grid">
+
+      </div>
+
+    </section>
+    <section class="last-transactions"></section>
+    <section class="add-transaction-form">
+      <div class="numbers-form"></div>
+    </section>
+    <nav class="bottom-nav"></nav>
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
