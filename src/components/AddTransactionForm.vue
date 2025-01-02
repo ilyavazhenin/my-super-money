0ab0f753528129b309bc819c00f5b857642eb79d
Ilya Vazhenin <heyheyftw@gmail.com>
2025-01-03 01:44:37 +0400

    feat: add pinia store for transactions form and submit button

--- src/components/AddTransactionForm.vue
+++ src/components/AddTransactionForm.vue
@@ -7,58 +7,81 @@
     import Button from 'primevue/button';
     import { ref } from 'vue';
 
+    import { useTransactionsStore } from '@/stores/transactions';
 
-    const amount = ref(null);
-    const transactionName = ref(null);
-    const date = ref(new Date());
-    const selectedCategory = ref(null);
-    const categories = ref([
-        { name: 'Food', icon: 'pi pi-apple' },
-        { name: 'Transport', icon: 'pi pi-car' },
-        { name: 'Entertainment', icon: 'pi pi-ticket' },
-        { name: 'Other', icon: 'pi pi-globe' },
-        { name: 'Home', icon: 'pi pi-home' },
-        { name: 'Health', icon: 'pi pi-heart' },
-        { name: 'Education', icon: 'pi pi-bookmark' },
-        { name: 'Clothes', icon: 'pi pi-tags' },
-        { name: 'Pets', icon: 'pi pi-discord' },
-        { name: 'Groceries', icon: 'pi pi-shopping-cart' },
-    ]);
+    const { transaction, categoryList, clearForm, } = useTransactionsStore();
+
+    // const amount = ref(null);
+    // const transactionName = ref(null);
+    // const date = ref(new Date());
+    
+    // const categories = ref([
+    //     { name: 'Food', icon: 'pi pi-apple' },
+    //     { name: 'Transport', icon: 'pi pi-car' },
+    //     { name: 'Entertainment', icon: 'pi pi-ticket' },
+    //     { name: 'Other', icon: 'pi pi-globe' },
+    //     { name: 'Home', icon: 'pi pi-home' },
+    //     { name: 'Health', icon: 'pi pi-heart' },
+    //     { name: 'Education', icon: 'pi pi-bookmark' },
+    //     { name: 'Clothes', icon: 'pi pi-tags' },
+    //     { name: 'Pets', icon: 'pi pi-discord' },
+    //     { name: 'Groceries', icon: 'pi pi-shopping-cart' },
+    // ]);
+    const selectedCategory = ref(categoryList[0]);
+
+    const scrollDownOnAmountClick = () => {
+        setTimeout(() => {
+            window.scrollTo({
+            top: window.scrollY + 150,
+            behavior: 'smooth'
+        });
+        }, 400);
+        
+    };
+
+    const handleSubmit = () => {
+        console.log(transaction.date, transaction.category, transaction.amount, transaction.name);
+        console.log('Submitted');
+        clearForm();
+        console.log(transaction.date, transaction.category, transaction.amount, transaction.name);
+    };
 
 </script>
 
 <template>
-    <div class="inputs-container">
-        <div class="fields-container">
-            <FloatLabel variant="in">
-                <InputNumber v-model="amount" mode="currency" currency="RUB" inputId="money_amount" locale="ru-RU" inputmode="numeric" fluid="false"/>
-                <label for="money_amount">Amount</label>
-            </FloatLabel>
+    <form @submit.prevent="handleSubmit">
+        <div class="inputs-container">
+            <div class="fields-container">
+                <FloatLabel variant="in">
+                    <InputNumber v-model="transaction.amount" mode="currency" currency="RUB" inputId="money_amount" locale="ru-RU" inputmode="numeric" fluid="false" @click="scrollDownOnAmountClick"/>
+                    <label for="money_amount">Amount</label>
+                </FloatLabel>
 
-            <FloatLabel variant="in">
-                <InputText id="transactionName" v-model="transactionName" class="transaction-input"/>
-                <label for="transactionName">Transaction name</label>
-            </FloatLabel>
+                <FloatLabel variant="in">
+                    <InputText id="transactionName" v-model="transaction.name" class="transaction-input"/>
+                    <label for="transactionName">Transaction name</label>
+                </FloatLabel>
 
-            <DatePicker class="date-picker-input" v-model="date" dateFormat="dd.mm.yy"/>
+                <DatePicker class="date-picker-input" v-model="transaction.date" dateFormat="dd.mm.yy"/>
+            </div>
+
+            <div class="categories-picker-container">
+                <Listbox v-model="transaction.category" :options="categoryList" defaultValue="categoryList[0]" optionLabel="name" class="categories-picker" listStyle="max-height:200px">
+                    <template #option="slotProps">
+                        <div class="category-item">
+                            <i :alt="slotProps.option.name" :class="slotProps.option.icon" style="width: 18px" />
+                            <div>{{ slotProps.option.name }}</div>
+                        </div>
+                    </template>
+                </Listbox>
+            </div>
         </div>
+        
 
-        <div class="categories-picker-container">
-            <Listbox v-model="selectedCategory" :options="categories" optionLabel="name" class="categories-picker" listStyle="max-height:200px">
-                <template #option="slotProps">
-                    <div class="category-item">
-                        <i :alt="slotProps.option.name" :class="slotProps.option.icon" style="width: 18px" />
-                        <div>{{ slotProps.option.name }}</div>
-                    </div>
-                </template>
-            </Listbox>
+        <div class="add-button-container">
+            <Button label="Submit" type="submit"/>
         </div>
-    </div>
-    
-
-    <div class="add-button-container">
-        <Button label="Submit" />
-    </div>
+    </form>
 
 </template>
 
